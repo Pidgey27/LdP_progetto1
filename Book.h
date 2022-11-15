87dbc83736ae92829833f15f0ad163dbb698757f
Aurora0895 <alevillara@gmail.com>
2022-11-15 17:54:58 +0100

    isbn first check
    check sul singolo carattere per numero o -, check sul numero di - nella string isbn

--- Book.h
+++ Book.h
@@ -14,10 +14,23 @@ public:
     book(std::string nome_autore, std::string cognome_Autore, std::string titolo, std::string isbn)
         : m_nome_Autore(nome_autore), m_cognome_Autore(cognome_Autore), m_titolo(titolo)
     {
-        //new std::string[4] substring;
+        if(!check_ISBN(isbn))
+            throw std::runtime_error ("Invalid ISBN code");
     }
     ~book();
     std::string getTitolo(){return m_titolo;}
     std::string getNomeAutore(){return m_nome_Autore;}
     std::string getCognomeAutore(){return m_cognome_Autore;}
+    bool check_ISBN(std::string isbn) {
+        const std::string num_check = isbn.substr(0, isbn.length() - 1);
+        int n_delimiters = 0;
+        for (char const &ch: num_check) {
+            if (!(std::isdigit(ch) && (ch != '-')))
+                return false;
+            if (ch == '-')
+                n_delimiters++;
+        }
+        if (n_delimiters != 3)
+            return false;
+    }
 };
